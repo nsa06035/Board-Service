cd8acf1d01c9c66a750fe06df00f3d15a8e1f349
jungeun <113340283+jungeunyooon@users.noreply.github.com>
2023-11-12 20:18:12 +0900

    Delete 윤정은/board-spring directory

--- 윤정은/board-spring/src/main/java/board/boardspring/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package board.boardspring.repository;
-
-import board.boardspring.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
